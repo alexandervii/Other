d911515e0512e433b53beff2913e5a53eb787039
pengbo <pengbosj@feinno.com>
2014-11-27 16:59:41 +0800

    togglebutton 基本功能完善

--- ToggleButton/src/com/alex/activity/MainActivity.java
+++ ToggleButton/src/com/alex/activity/MainActivity.java
@@ -1,26 +1,37 @@
 package com.alex.activity;
 
-import com.alex.R;
-import com.alex.R.layout;
-import com.alex.R.menu;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.alex.R;
+import com.alex.widget.ToggleButton;
+import com.alex.widget.ToggleButton.OnStatusChangeListener;
+import com.alex.widget.ToggleButton.Status;
 
 public class MainActivity extends Activity {
 
+	private ToggleButton mToggleButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initView();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+	private void initView() {
+		mToggleButton = (ToggleButton) findViewById(R.id.togglebutton);
+		mToggleButton.setOnStatusChangeListener(new OnStatusChangeListener() {
+			@Override
+			public void onChange(Status status) {
+				if(status == Status.OFF) {
+					Toast.makeText(MainActivity.this, "关闭", 0).show();
+				} else if(status == Status.ON) {
+					Toast.makeText(MainActivity.this, "打开", 0).show();
+				}
+			}
+		});
 	}
 
 }
--- ToggleButton/src/com/alex/widget/ToggleButton.java
+++ ToggleButton/src/com/alex/widget/ToggleButton.java
@@ -4,197 +4,259 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class ToggleButton extends View {
+	
 	public static final String TAG = ToggleButton.class.getSimpleName();
+
+	private float mRadiusLeft;
+	private float mRadiusRight;
 	
-	public static final int STATUS_DRAGING = 0;
-	public static final int STATUS_SWITCH_ON = 1;
-	public static final int STATUS_SWITCH_OFF = 2;
+	private float mLeft;
+	private float mRight;
+	private float mTop;
+	private float mBottom;
 	
-	private int mStatus = STATUS_SWITCH_ON;
-	private String mContent = "demacial";
+	private float mCenterX;
 	
+	private float mTextX;
+	private float mTextY;
+	private float mTextSize;
 	
+	private float mBackWidth;
+	private float mBackHeight;
+	
+	private float mRadiusX;
+	private float mRadiusY;
+	private float mRadius;
+	
+	private Paint mDragPaint;
 	private Paint mBackPaint;
-	private Paint mBtnPaint;
 	private Paint mTextPaint;
 	
-	private RectF mBackRect;
+	private float mOldX;
+	private float mOldY;
 	
-	private int mBackWidth = 80;
-	private int mBackHeight = 80;
+	private int mScreenWidth;
+	private int mScreenHeight;
 	
-	private int mGap = 5;
+	private Status mStatus;
+	private Drag mDragType = Drag.TOUCH;
 	
-	private float mMoveX = 0;
-	private float mLastX = 0;
-	private float mDx = 0;
+	private float mTouchX,mTouchY;
 	
-	/** 背景的边界*/
-	private float mTop = 30;
-	private float mLeft = 30;
-	private float mRight = mLeft + mBackWidth + mBackHeight;
-	private float mBottom = mTop + mBackHeight;
 	
-	/** 圆形按钮的半径*/
-	private int mButtonRadius = mBackHeight/2;
-	
-	/** 左边按钮的中心X坐标*/
-	private float mBtnLX = mLeft + mButtonRadius;
-	/** 左边按钮的中心Y坐标*/
-	private float mBtnLY = mTop + mButtonRadius;
-	/**右边按钮的中心X坐标*/
-	private float mBtnRX = mRight - mButtonRadius;
-	/**右边按钮的中心Y坐标*/
-	private float mBtnRY = mTop + mButtonRadius;
-	/** 背景的中心X坐标*/
-	private float mBackCenterX = (mBtnLX + mBtnRX) / 2;
-	/** 背景字体的大小*/
-	private float mTextSize = 22;
-	/** 字体的Y*/
-	private float mTextY = mBtnLY + mGap;
-	/**字体的X*/
-	private float mTextX = (mRight - mBackHeight + mLeft - mTextSize)/2 - mGap;
+	private OnStatusChangeListener mOnStatusChangeListener;
 	
 	public ToggleButton(Context context) {
 		super(context);
-		init();
+		initView();
+	}
+	
+	public ToggleButton(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		initView();
 	}
 
 	public ToggleButton(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		init();
+		initView();
 	}
 
-	public ToggleButton(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		init();
-	}
-	
-	private void init() {
+	private void initView() {
+		mDragPaint = new Paint();
 		mBackPaint = new Paint();
-		mBtnPaint = new Paint();
 		mTextPaint = new Paint();
-		mBackRect = new RectF();
+		mScreenWidth = getResources().getDisplayMetrics().widthPixels;
+		mScreenHeight = getResources().getDisplayMetrics().heightPixels;
+		initData();
 	}
 	
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	private void initData() {
+		/*******需要初始化的参数*********/
+		mRadius = 50;
+		mBackWidth = 130;
+		mLeft = 50;
+		mTop = 50;
+		mStatus = Status.OFF;
+		mTextSize = 35;
+		/*******需要初始化的参数*********/
+		
+		mRadiusLeft = mLeft + mRadius;
+		mRadiusRight = mRadiusLeft + mBackWidth;
+		
+		mBackHeight = mRadius * 2;
+		mBottom = mTop + mBackHeight;
+		
+		mRadiusX = mRadiusLeft;
+		mRadiusY = mTop + mRadius;
+		
+		mRight = mRadiusRight + mRadius;
+		
+		mCenterX = mRadiusLeft + mBackWidth / 2;
+		
+		mTextX = mLeft + mBackWidth / 2 - mTextSize;
+		mTextY = mTop + mBackHeight / 2 + mTextSize / 3;
 	}
-	
-	@Override
-	protected void onLayout(boolean changed, int left, int top, int right,
-			int bottom) {
-		super.onLayout(changed, left, top, right, bottom);
-	}
-	
+
 	@Override
 	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		drawBackground(canvas, mStatus);
-		drawButton(canvas, mStatus, mMoveX);
+		drawBackground(canvas);
+		drawText(canvas);
+		drawButton(canvas);
 	}
 	
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		float x = event.getX();
-		float y = event.getY();
-		float lastX = -1;
-		switch (event.getAction()) {
-		case MotionEvent.ACTION_DOWN:
-			//如果手点到了背景界内才会出反应
-			if(x < mLeft || x > mRight || y < mTop || y > mBottom) {
-				return false;
-			}
-			lastX = event.getX();
-			mLastX = lastX;
-			mStatus = STATUS_DRAGING;
-			break;
-		case MotionEvent.ACTION_MOVE:
-			float currentX = event.getX();
-			//如果是滑动状态
-			//根据currentX - lastX的正负，及绝对值来计算出滑块的移动方向和距离
-			//如果出界(x轴边界)了，做出出界处理
-			
-			if(mStatus == STATUS_DRAGING) {
-				mDx = currentX - lastX;
-				mMoveX = mLastX + mDx;
-				if(mMoveX > mBtnRX) {
-					mMoveX = mBtnRX;
-				}
-				if(mMoveX < mBtnLX) {
-					mMoveX = mBtnLX;
-				}
-			}
-			lastX = currentX;
-			mLastX = mMoveX;
-			break;
-		case MotionEvent.ACTION_UP:
-			//对抬起时的点左边界检测，如果x坐标小于背景中点x，则滑块最终在左边，否则在右边
-			//根据和上面的判断，重置滑块的状态
-			
-			if(mLastX >= mBackCenterX) {
-				mMoveX = mBtnRX;
-				mStatus = STATUS_SWITCH_ON;
-			} else {
-				mMoveX = mBtnLX;
-				mStatus = STATUS_SWITCH_OFF;
-			}
-			break;
-		}
-		Log.i(TAG, "onTouchEvent-->mLastX-->>"+mLastX);
-		Log.i(TAG, "onTouchEvent-->mMoveX-->>"+mMoveX);
-		invalidate();
-		return super.onTouchEvent(event);
-	}
-	
-	private void drawButton(Canvas canvas, int status, float centerX) {
-		mBtnPaint.setColor(Color.RED);
-		if(status != STATUS_DRAGING) {
-			if(status == STATUS_SWITCH_ON) {
-				centerX = mBtnRX;
-			} else if(status == STATUS_SWITCH_OFF) {
-				centerX = mBtnLX;
-			}
-		} 
-		canvas.drawCircle(centerX, mBtnRY, mButtonRadius, mBtnPaint);
-	}
-	
-	private void drawBackground(Canvas canvas, int status) {
-		mBackPaint.setColor(Color.BLUE);
-		mBackRect.set(mBtnLX, mTop, mBtnRX, mBottom);
-		canvas.drawRect(mBackRect, mBackPaint);
-		canvas.drawCircle(mBtnLX, mBtnLY, mButtonRadius, mBackPaint);
-		canvas.drawCircle(mBtnRX, mBtnRY, mButtonRadius, mBackPaint);
-		drawText(canvas, status);
-	}
-	
-	private void drawText(Canvas canvas, int status) {
+	private void drawText(Canvas canvas) {
+		mTextPaint.setColor(Color.BLACK);
 		mTextPaint.setTextSize(mTextSize);
-		if(status == STATUS_SWITCH_ON) {
-			canvas.drawText(mContent, mTextX, mTextY, mTextPaint);
-		} 
+		if(mRadiusX <= mTextX + mRadius) {
+			return ;
+		}
+		if(mStatus == Status.ON) {
+			canvas.drawText("Open", mTextX, mTextY, mTextPaint);
+		}
+	}
+
+	private void drawBackground(Canvas canvas) {
+		if(mStatus == Status.OFF) {
+			mBackPaint.setColor(Color.GRAY);
+		} else if(mStatus == Status.ON) {
+			mBackPaint.setColor(Color.BLUE);
+		}
+		canvas.drawCircle(mRadiusLeft, mRadiusY, mRadius, mBackPaint);
+		canvas.drawCircle(mRadiusRight, mRadiusY, mRadius, mBackPaint);
+		canvas.drawRect(mRadiusLeft, mTop, mRadiusRight, mBottom, mBackPaint);
+	}
+
+	public void drawButton(Canvas canvas) {
+		mDragPaint.setColor(Color.RED);
+		canvas.drawCircle(mRadiusX, mRadiusY, mRadius, mDragPaint);
+	}
+	
+	public enum Status {
+		//开
+		ON,
+		//关
+		OFF;
+	}
+	
+	public enum Drag {
+		//默认为点击到按钮状态
+		TOUCH,
+		//没有点击到按钮
+		UN_TOUCH,
+	}
+	
+	public void setOnStatusChangeListener(OnStatusChangeListener listener) {
+		mOnStatusChangeListener = listener;
+	}
+	
+	public interface OnStatusChangeListener {
+		public void onChange(Status status);
 	}
 	
 	/**
-	 * set the status to be on or off
-	 * @param status
+	 * 检查第一个点是否在以第二个点为中心，半径为radius的圆上
+	 * @param fx 第一个点的x坐标
+	 * @param fy 第一个点的y坐标
+	 * @param nx 第二个点的x坐标
+	 * @param ny 第二个点的y坐标
+	 * @return true表示在圆上，false表示不在
 	 */
-	public void setStatus(int status) {
-		switch (status) {
-		case STATUS_SWITCH_OFF:
-			this.mStatus = STATUS_SWITCH_OFF;
+	private boolean isOnCircle(float fx,float fy, float nx, float ny, float radius) {
+		double powerRadius = Math.pow((fx - nx),2) + Math.pow((fy - ny), 2);
+		if(powerRadius >= Math.pow(radius,2)) {
+			return false;
+		}
+		return true;
+	}
+	
+	public Status getStatus() {
+		return mStatus;
+	}
+	
+	public void setStatus(Status status) {
+		mStatus = status;
+		postInvalidate();
+	}
+	
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			mDragType = Drag.TOUCH;
+			mTouchX = -1;
+			mTouchY = -1;
+			mOldX = event.getX();
+			mOldY = event.getY();
+			if(!isOnCircle(mOldX,mOldY,mRadiusX,mRadiusY,mRadius)) {
+				Log.i(TAG, "down--->>>未点中按钮");
+				mDragType = Drag.UN_TOUCH;
+				mTouchX = mOldX;
+				mTouchY = mOldY;
+				Log.i(TAG, "mTouchX ="+mTouchX);
+				Log.i(TAG, "mTouchY ="+mTouchY);
+			}
 			break;
-		case STATUS_SWITCH_ON:
-			this.mStatus = STATUS_SWITCH_ON;
+		case MotionEvent.ACTION_MOVE:
+			float x = event.getX();
+			float y = event.getY();
+			if(mDragType == Drag.UN_TOUCH && x != mTouchX) {
+				return true;
+			}
+			mRadiusX += x - mOldX;
+//			mRadiusY += y - mOldY;
+			if(mRadiusX < mRadiusLeft) {
+				mRadiusX = mRadiusLeft;
+			}
+			if(mRadiusX >= mRadiusRight) {
+				mRadiusX = mRadiusRight;
+			}
+			mOldX = x;
+			mOldY = y;
+			Log.i(TAG, "move");
+			break;
+		case MotionEvent.ACTION_UP:
+			mOldX = event.getX();
+			mOldY = event.getY();
+			if(mDragType == Drag.TOUCH) {
+				Log.i(TAG, "up---->>>拖动");
+				//拖动转换状态
+				if(mRadiusX < mCenterX) {
+					Log.i(TAG, "拖动了->关");
+					mRadiusX = mRadiusLeft;
+					mStatus = Status.OFF;
+				} else {
+					Log.i(TAG, "拖动了->开");
+					mRadiusX = mRadiusRight;
+					mStatus = Status.ON;
+				}
+			} else if(mDragType == Drag.UN_TOUCH && mOldX == mTouchX && mOldY == mTouchY) {
+				//是否点击的on
+				if(isOnCircle(mOldX,mOldY,mRadiusRight,mRadiusY,mRadius)) {
+					Log.i(TAG, "点击了->开");
+					mRadiusX = mRadiusRight;
+					mStatus = Status.ON; 
+				} 
+				//是否点击的off
+				if(isOnCircle(mOldX,mOldY,mRadiusLeft,mRadiusY,mRadius)) {
+					Log.i(TAG, "点击了->关");
+					mRadiusX = mRadiusLeft;
+					mStatus = Status.OFF;
+				}
+			} 
+			if(mOnStatusChangeListener != null) {
+				mOnStatusChangeListener.onChange(mStatus);
+			}
 			break;
 		}
+		postInvalidate();
+		return true;
 	}
+	
 }
