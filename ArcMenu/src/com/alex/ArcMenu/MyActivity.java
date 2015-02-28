3c9120430aae15e224d2adf9554dc16fe3c46e1f
pengbo <pengbosj@feinno>
2015-02-28 15:50:28 +0800

    完善ArcMenu代码

--- ArcMenu/src/com/alex/ArcMenu/MyActivity.java
+++ ArcMenu/src/com/alex/ArcMenu/MyActivity.java
@@ -1,15 +1,83 @@
 package com.alex.ArcMenu;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.alex.ArcMenu.view.ArcMenu;
+import com.alex.ArcMenu.view.ArcMenu.OnMenuItemClickListener;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+	
+	private ListView mListView;
+	private List<String> mData;
+	private ArcMenu mRightBottomMenu;
+	private ArcMenu mLeftBottomMenu;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initView();
+        initData();
+        initEvent();
     }
+    
+    
+
+	private void initEvent() {
+		mListView.setOnScrollListener(new OnScrollListener() {
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+			}
+			
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+				if(mRightBottomMenu.isOpen()) {
+					mRightBottomMenu.toggleMenu(600);
+				}
+				if(mLeftBottomMenu.isOpen()) {
+					mLeftBottomMenu.toggleMenu(600);
+				}
+			}
+		});
+		mRightBottomMenu.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+			@Override
+			public void onClick(View view, int position) {
+				Toast.makeText(getApplicationContext(), position+":"+view.getTag(), 1).show();
+			}
+		});
+		
+		mLeftBottomMenu.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+			@Override
+			public void onClick(View view, int position) {
+				Toast.makeText(getApplicationContext(), position+":"+view.getTag(), 1).show();
+			}
+		});
+	}
+
+	private void initView() {
+		mListView = (ListView) findViewById(R.id.id_listview);
+		mRightBottomMenu = (ArcMenu) findViewById(R.id.id_right_bottom_menu);
+		mLeftBottomMenu = (ArcMenu) findViewById(R.id.id_left_bottom_menu);
+	}
+
+	private void initData() {
+		mData = new ArrayList<String>();
+		for(int i='A'; i<'Z';i++) {
+			mData.add((char)i+"");
+		}
+		mListView.setAdapter(new ArrayAdapter<String>(this, 
+				android.R.layout.simple_list_item_1, 
+				mData));
+	}
 }
--- ArcMenu/src/com/alex/ArcMenu/view/ArcMenu.java
+++ ArcMenu/src/com/alex/ArcMenu/view/ArcMenu.java
@@ -7,6 +7,14 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+
 import com.alex.ArcMenu.R;
 
 /**
@@ -87,6 +95,29 @@ public class ArcMenu extends ViewGroup implements View.OnClickListener {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         if(changed) {
             layoutMainButton();
+            int childCount = getChildCount();
+            for(int i=0; i<childCount - 1; i++) {
+            	View child = getChildAt(i+1);
+            	
+            	child.setVisibility(View.GONE);
+            	
+            	int cl = (int) (mRadius*Math.sin(Math.PI/2/(childCount - 2)*i));
+            	int ct = (int) (mRadius*Math.cos(Math.PI/2/(childCount - 2)*i));
+            	
+            	int cWidth = child.getMeasuredWidth();
+            	int cHeight = child.getMeasuredHeight();
+            	
+            	//如果位置在左下，右下
+            	if(mPosition == Position.LEFT_BOTTOM || mPosition == Position.RIGHT_BOTTOM) {
+            		ct = getMeasuredHeight() - cHeight - ct;
+            	}
+            	//如果位置在右上，右下
+            	if(mPosition == Position.RIGHT_TOP || mPosition == Position.RIGHT_BOTTOM) {
+            		cl = getMeasuredWidth() - cWidth - cl;
+            	}
+            	
+            	child.layout(cl, ct, cl+cWidth, ct+cHeight);
+            }
         }
     }
 
@@ -118,9 +149,187 @@ public class ArcMenu extends ViewGroup implements View.OnClickListener {
         }
         mMainButton.layout(l,t,l+width,t+height);
     }
+    
+    public boolean isOpen() {
+    	return mStatus == Status.OPEN ? true : false;
+    }
 
     @Override
     public void onClick(View v) {
-
+//    	mMainButton = findViewById(R.id.id_button);
+//    	if(mMainButton == null) {
+//    		mMainButton = getChildAt(0);
+//    	}
+    	rotateMainButton(v,0f,360f,300);
+    	toggleMenu(300);
     }
+
+    /**
+     * 切换菜单
+     */
+	public void toggleMenu(int duration) {
+		//为所有的menu item添加平移动画和旋转动画
+		int childCount = getChildCount();
+		for(int i=0; i<childCount - 1; i++) {
+			final View childView = getChildAt(i + 1);
+			
+			//针对当子按钮为隐藏状态，执行动画需要显示按钮
+			childView.setVisibility(View.VISIBLE);
+			
+			//end 0,0
+			//start
+			int cl = (int) (mRadius*Math.sin(Math.PI/2/(childCount - 2)*i));//TODO
+        	int ct = (int) (mRadius*Math.cos(Math.PI/2/(childCount - 2)*i));
+        	
+        	int xFlag = 1;
+        	int yFlag = 1;
+        	
+        	if(mPosition == Position.LEFT_TOP || mPosition == Position.LEFT_BOTTOM) {
+        		xFlag = -1;
+        	}
+        	if(mPosition == Position.LEFT_TOP || mPosition == Position.RIGHT_TOP) {
+        		yFlag = -1;
+        	}
+			
+        	AnimationSet animateSet = new AnimationSet(true);
+        	Animation transAnim = null;
+        	//to open
+        	if(mStatus == Status.CLOSE) {
+        		transAnim = new TranslateAnimation(xFlag*cl,0,yFlag*ct,0);
+        		childView.setClickable(true);
+        		childView.setFocusable(true);
+        	} else {//to close
+        		transAnim = new TranslateAnimation(0,xFlag*cl,0,yFlag*ct);
+        		childView.setClickable(false);
+        		childView.setFocusable(false);
+        	}
+        	transAnim.setFillAfter(true);
+        	transAnim.setDuration(duration);
+        	transAnim.setStartOffset((i*100) / childCount);
+        	
+        	transAnim.setAnimationListener(new AnimationListener() {
+				@Override
+				public void onAnimationStart(Animation animation) {
+					
+				}
+				
+				@Override
+				public void onAnimationRepeat(Animation animation) {
+					
+				}
+				
+				@Override
+				public void onAnimationEnd(Animation animation) {
+					//关闭动画结束后子按钮隐藏
+					if(mStatus == Status.CLOSE) {
+						childView.setVisibility(View.GONE);
+					}
+				}
+			});
+        	
+        	//旋转动画
+        	RotateAnimation rotateAnim = new RotateAnimation(0, 720,
+    				Animation.RELATIVE_TO_SELF, 0.5f, 
+    				Animation.RELATIVE_TO_SELF, 0.5f);
+        	rotateAnim.setDuration(duration);
+        	rotateAnim.setFillAfter(true);
+    		
+        	animateSet.addAnimation(rotateAnim);
+    		animateSet.addAnimation(transAnim);
+    		
+    		childView.startAnimation(animateSet);
+    		final int pos = i + 1;
+    		childView.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					if(mOnMenuItemClickListener != null) {
+						mOnMenuItemClickListener.onClick(childView, pos);
+					}
+					menuItemAnim(pos-1);
+					changeStatus();
+				}
+			});
+		}
+		//切换菜单状态
+		changeStatus();
+	}
+	
+	/**
+	 * menu item的点击动画
+	 * @param pos
+	 */
+	private void menuItemAnim(int pos) {
+		for(int i=0; i<getChildCount() - 1; i++) {
+			View childView = getChildAt(i+1);
+			if(i == pos) {
+				childView.startAnimation(scaleBigAnim(300));
+			} else {
+				childView.startAnimation(scaleSmallAnim(300));
+			}
+			childView.setClickable(false);
+			childView.setFocusable(false);
+		}
+	}
+
+	/**
+	 * 为其它未点击的item设置缩小和透明度降低动画
+	 * @param duration
+	 * @return
+	 */
+	private Animation scaleSmallAnim(int duration) {
+		AnimationSet animateSet = new AnimationSet(true);
+		
+		ScaleAnimation scaleAnim = new ScaleAnimation(1.0f, 0.0f, 1.0f, 0.0f, 
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+		AlphaAnimation alphaAnim = new AlphaAnimation(1f, 0);
+		
+		animateSet.addAnimation(scaleAnim);
+		animateSet.addAnimation(alphaAnim);
+		
+		animateSet.setDuration(duration);
+		animateSet.setFillAfter(true);
+		
+		return animateSet;
+	}
+
+	/**
+	 * 为当前点击的item设置变大和透明度降低的动画
+	 * @param duration
+	 * @return
+	 */
+	private Animation scaleBigAnim(int duration) {
+		AnimationSet animateSet = new AnimationSet(true);
+		
+		ScaleAnimation scaleAnim = new ScaleAnimation(1.0f, 4.0f, 1.0f, 4.0f, 
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+		AlphaAnimation alphaAnim = new AlphaAnimation(1f, 0);
+		
+		animateSet.addAnimation(scaleAnim);
+		animateSet.addAnimation(alphaAnim);
+		
+		animateSet.setDuration(duration);
+		animateSet.setFillAfter(true);
+		
+		return animateSet;
+	}
+
+	private void changeStatus() {
+		mStatus = mStatus == Status.CLOSE ? Status.OPEN : Status.CLOSE; 
+	}
+
+	/**
+	 * 主按钮的旋转动画
+	 * @param v
+	 * @param start
+	 * @param end
+	 * @param duration
+	 */
+	private void rotateMainButton(View v, float start, float end, int duration) {
+		RotateAnimation ra = new RotateAnimation(start, end,
+				Animation.RELATIVE_TO_SELF, 0.5f, 
+				Animation.RELATIVE_TO_SELF, 0.5f);
+		ra.setDuration(duration);
+		ra.setFillAfter(true);
+		v.startAnimation(ra);
+	}
 }
