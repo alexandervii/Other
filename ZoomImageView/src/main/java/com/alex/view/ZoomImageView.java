2cec3447b9d07bc10e129a1d74cef01e85562776
Alexandervii <alexandervii@163.com>
2015-04-09 12:12:25 +0800

    modify ZoomImageView

--- ZoomImageView/src/main/java/com/alex/view/ZoomImageView.java
+++ ZoomImageView/src/main/java/com/alex/view/ZoomImageView.java
@@ -2,11 +2,15 @@ package com.alex.view;
 
 import android.content.Context;
 import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
@@ -16,16 +20,41 @@ import android.widget.ImageView;
 public class ZoomImageView extends ImageView implements ViewTreeObserver.OnGlobalLayoutListener, ScaleGestureDetector.OnScaleGestureListener,View.OnTouchListener {
 
     private boolean mOnce = false;
-    //初始化时缩放的值
+    /*初始化时缩放的值*/
     private float mInitScale;
-    //双击放大时到达的值
+    /*双击放大时到达的值*/
     private float mMidScale;
-    //放大的极限
+    /*放大的极限*/
     private float mMaxScale;
-    //缩放和平移的矩阵
+    /*缩放和平移的矩阵*/
     private Matrix mScaleMatrix;
-    //多点触控
+    /*多点触控*/
     private ScaleGestureDetector mScaleGestureDetector;
+
+
+    /*自由移动*/
+    /*记录上次多点触控的数量*/
+    private int mLastPointerCount;
+
+    /*上次移动的x坐标*/
+    private float mLastX;
+    /*上次移动的y坐标*/
+    private float mLastY;
+
+    /*判断是否移动的最小单位*/
+    private int mTouchSlop;
+    /*是否能拖动*/
+    private boolean isCanDrag;
+    /*是否能左右移动*/
+    private boolean isCheckLeftAndRight;
+    /*是否能上下移动*/
+    private boolean isCheckTopAndBottom;
+
+
+    /*双击放大和缩小*/
+    private GestureDetector mGestureDetector;
+    /*是否已经在缩放了*/
+    private boolean isAutoScale;
 
 
     public ZoomImageView(Context context) {
@@ -42,6 +71,85 @@ public class ZoomImageView extends ImageView implements ViewTreeObserver.OnGloba
         super.setScaleType(ScaleType.MATRIX);
         mScaleGestureDetector  = new ScaleGestureDetector(context,this);
         setOnTouchListener(this);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        mGestureDetector = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onDoubleTap(MotionEvent e) {
+                        if(isAutoScale) {
+                            return true;
+                        }
+                        float x = e.getX();
+                        float y = e.getY();
+                        //如果当前放大值小于mMidScale，则放大到mMidScale
+                        //否则缩小到mInitScale
+                        float scale;
+                        if(getScale() < mMidScale) {
+//                            scale = mMidScale / getScale();
+//                            mScaleMatrix.postScale(scale,scale,x,y);
+//                            setImageMatrix(mScaleMatrix);
+                            scale = mMidScale;
+                        } else {
+//                            scale = mInitScale / getScale();
+//                            mScaleMatrix.postScale(scale,scale,x,y);
+//                            setImageMatrix(mScaleMatrix);
+                            scale = mInitScale;
+                        }
+                        //进行梯度缩放
+                        postDelayed(new AutoScaleRunnable(scale,x,y),16);
+                        isAutoScale = true;
+                        return true;
+                    }
+                });
+    }
+
+    private class AutoScaleRunnable implements Runnable {
+
+        /*缩放的目标值*/
+        private float mTargetScale;
+        /*缩放的中心点*/
+        private float x;
+        private float y;
+        /*放大的梯度*/
+        private final float BIGGER = 1.07F;
+        /*缩小的梯度*/
+        private final float SMALLER = 0.93F;
+
+        private float mTempScale;
+
+        private AutoScaleRunnable(float targetScale, float x, float y) {
+            this.mTargetScale = targetScale;
+            this.x = x;
+            this.y = y;
+
+            if(getScale() < mTargetScale) {
+                mTempScale = BIGGER;
+            } else if(getScale() > mTargetScale) {
+                mTempScale = SMALLER;
+            }
+        }
+
+        @Override
+        public void run() {
+            //进行缩放
+            mScaleMatrix.postScale(mTempScale,mTempScale,x,y);
+            checkBorderAndCenterWhenScale();
+            setImageMatrix(mScaleMatrix);
+
+            float currentScale = getScale();
+
+            //如果为达到目标值，则继续
+            if((mTempScale < 1.0f && currentScale > mTargetScale) ||
+                    (mTempScale > 1.0f && currentScale < mTargetScale)) {
+                postDelayed(this,16);
+            } else {// 设置为目标值
+                float scale = mTargetScale / currentScale;
+                mScaleMatrix.postScale(scale,scale,x,y);
+                checkBorderAndCenterWhenScale();
+                setImageMatrix(mScaleMatrix);
+                isAutoScale = false;
+            }
+        }
     }
 
     /**
@@ -69,7 +177,7 @@ public class ZoomImageView extends ImageView implements ViewTreeObserver.OnGloba
             if(dh > height && dw < width) {
                 scale = height * 1.0f / dh;
             }
-            //如果图片的宽，高度均大于或者小于控件的宽高度，则取宽高的比值较小值作为缩放比进行缩小
+            //如果图片的宽，高度均大于或者小于控件的宽高度，则取宽高的比值较小值作为缩放比进行缩小或者放大
             if((dw > width && dh > height) || (dw < width && dh < height)) {
                 scale = Math.min(width * 1.0f / dw, height * 1.0f / dh);
             }
@@ -116,7 +224,7 @@ public class ZoomImageView extends ImageView implements ViewTreeObserver.OnGloba
     }
 
     @Override
-    public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+    public boolean onScale(ScaleGestureDetector detector) {
         float scale = getScale();
         float factor = mScaleGestureDetector.getScaleFactor();
         if(getDrawable() == null) {
@@ -133,10 +241,66 @@ public class ZoomImageView extends ImageView implements ViewTreeObserver.OnGloba
             if(scale * factor > mMaxScale) {
                 factor = mMaxScale / scale;
             }
-            mScaleMatrix.postScale(factor,factor,getWidth()/2,getHeight()/2);
+            //以屏幕中心点为基准放大
+//            mScaleMatrix.postScale(factor,factor,getWidth()/2,getHeight()/2);
+            //以触控的中心点为基准放大
+            mScaleMatrix.postScale(factor,factor, detector.getFocusX(),detector.getFocusY());
+            //在缩放的同时，图片可能出现白边，或者不居中，则需缩放的同时进行调整
+            checkBorderAndCenterWhenScale();
             setImageMatrix(mScaleMatrix);
         }
         return true;
+    }
+
+    /**
+     * 获得图片放大缩小以后的宽高，以及left,top,right,bottom;
+     * @return
+     */
+    private RectF getMatrixRectF() {
+        Matrix matrix = mScaleMatrix;
+        RectF rectF = new RectF();
+        Drawable d = getDrawable();
+        if(d != null) {
+            rectF.set(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+            matrix.mapRect(rectF);
+        }
+        return rectF;
+    }
+
+    /**
+     * 在缩放时进行边界控制以及位置的控制
+     */
+    private void checkBorderAndCenterWhenScale() {
+        RectF rectF = getMatrixRectF();
+        float deltaX = 0;
+        float deltaY = 0;
+
+        int width = getWidth();
+        int height = getHeight();
+        if(rectF.width() >= width) {
+            if(rectF.left > 0) {
+                deltaX = -rectF.left;
+            }
+            if(rectF.right < width) {
+                deltaX = width - rectF.right;
+            }
+        }
+        if(rectF.height() >= height) {
+            if(rectF.top > 0) {
+                deltaY = -rectF.top;
+            }
+            if(rectF.bottom < height) {
+                deltaY = height - rectF.bottom;
+            }
+        }
+        //如果图片的宽或者高小于控件的宽或者高，则让图片居中
+        if(rectF.width() < width) {
+           deltaX = width/2f - rectF.right + rectF.width()/2f;
+        }
+        if(rectF.height() < height) {
+            deltaY = height/2f - rectF.bottom + rectF.height()/2f;
+        }
+        mScaleMatrix.postTranslate(deltaX, deltaY);
     }
 
     @Override
@@ -151,7 +315,116 @@ public class ZoomImageView extends ImageView implements ViewTreeObserver.OnGloba
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
+        //这里是防止双击造成的移动操作
+        if(mGestureDetector.onTouchEvent(motionEvent)) {
+            return true;
+        }
         mScaleGestureDetector.onTouchEvent(motionEvent);
+        float x = 0;
+        float y = 0;
+        //拿到多点触控的数量
+        int pointerCount = motionEvent.getPointerCount();
+        //获取多点触控的中心点的位置
+        for(int i=0; i<pointerCount; i++) {
+            x += motionEvent.getX(i);
+            y += motionEvent.getY(i);
+        }
+        x /= pointerCount;
+        y /= pointerCount;
+        if(mLastPointerCount != pointerCount) {
+            mLastX = x;
+            mLastY = y;
+            isCanDrag = false;
+        }
+        mLastPointerCount = pointerCount;
+        RectF rectF = getMatrixRectF();
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                //如果当前图片是放大的效果，则不能被父类拦截事件（+0.01是为了防止边界图片的宽高刚好比空间宽高大0.0X）
+                if(rectF.width() > getWidth() + 0.01 || rectF.height() > getHeight() + 0.01) {
+                    if(getParent() instanceof ViewPager) {
+                        getParent().requestDisallowInterceptTouchEvent(true);
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if(rectF.width() > getWidth() + 0.01 || rectF.height() > getHeight() + 0.01) {
+                    if(getParent() instanceof ViewPager) {
+                        getParent().requestDisallowInterceptTouchEvent(true);
+                    }
+                }
+                float dx = x - mLastX;
+                float dy = y - mLastY;
+                if(!isCanDrag) {
+                    isCanDrag = isMove(dx,dy);
+                }
+                if(isCanDrag) {
+                    if(getDrawable() != null) {
+                        isCheckLeftAndRight = isCheckTopAndBottom = true;
+                        //如果宽度小于控件宽度，不允许左右移动
+                        if(rectF.width() <= getWidth()) {
+                            isCheckLeftAndRight = false;
+                            dx = 0;
+                        }
+                        //如果高度小于控件高度，不允许上下移动
+                        if(rectF.height() <= getHeight()) {
+                            isCheckTopAndBottom = false;
+                            dy = 0;
+                        }
+                        mScaleMatrix.postTranslate(dx, dy);
+                        checkBorderWhenTranslate(dx, dy);
+                        setImageMatrix(mScaleMatrix);
+                    }
+                }
+                mLastY = y;
+                mLastX = x;
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                mLastPointerCount = 0;
+                break;
+        }
         return true;
+    }
+
+    /**
+     * 当移动时，进行边界检查
+     */
+    private void checkBorderWhenTranslate(float dx, float dy) {
+        RectF rectF = getMatrixRectF();
+
+        float deltaX = 0;
+        float deltaY = 0;
+
+        int width = getWidth();
+        int height = getHeight();
+
+        //如果图片的顶端是大于零，并且图片高度大于控件高度，则向上偏移，直至图片顶端到达控件顶端
+        if(rectF.top > 0 && isCheckTopAndBottom) {
+            deltaY = -rectF.top;
+        }
+        //如果图片的底端是小于控件高度，并且图片高度大于控件高度，则向下偏移，直至图片底端到达控件底端
+        if(rectF.bottom < height && isCheckTopAndBottom) {
+            deltaY = height - rectF.bottom;
+        }
+        //如果图片的左端大于零，并且图片宽度大于控件宽度，则向左偏移，直至图片左端到达控件左端
+        if(rectF.left > 0 && isCheckLeftAndRight) {
+            deltaX = -rectF.left;
+        }
+        //如果图片的右端小于控件宽度，并且图片宽度大于控件宽度，则向右偏移，直至图片右端到达控件右端
+        if(rectF.right < width && isCheckLeftAndRight) {
+            deltaX = width - rectF.right;
+        }
+        mScaleMatrix.postTranslate(deltaX,deltaY);
+    }
+
+    /**
+     * 判断是否移动了
+     * @param dx
+     * @param dy
+     * @return
+     */
+    private boolean isMove(float dx, float dy) {
+        return Math.sqrt(dx*dx + dy*dy) > mTouchSlop;
     }
 }
--- ZoomImageView/src/main/java/com/alex/ziv/MainActivity.java
+++ ZoomImageView/src/main/java/com/alex/ziv/MainActivity.java
@@ -2,38 +2,66 @@ package com.alex.ziv;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.alex.view.ZoomImageView;
 
 
 public class MainActivity extends Activity {
+
+    private ViewPager mViewPager;
+
+    private int[] res = new int[] {
+            R.mipmap.a,
+            R.mipmap.b,
+            R.mipmap.c,
+            R.mipmap.d,
+            R.mipmap.e,
+            R.mipmap.f,
+            R.mipmap.g
+    };
+
+    private ImageView[] mImgs = new ImageView[res.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mViewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return res.length;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object o) {
+                return view == o;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                ZoomImageView img = new ZoomImageView(getApplicationContext());
+                img.setImageResource(res[position]);
+                container.addView(img);
+                mImgs[position] = img;
+                return img;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(mImgs[position]);
+            }
+        });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
