b7d7020fc19514953041b580a0ed50fca8f86c19
pengbo <pengbosj@feinno>
2015-04-13 18:53:12 +0800

    modify downloader

--- Downloader/src/main/java/com/alex/service/DownloadService.java
+++ Downloader/src/main/java/com/alex/service/DownloadService.java
@@ -3,11 +3,22 @@ package com.alex.service;
 import android.app.Service;
 import android.content.Intent;
 import android.nfc.Tag;
+import android.os.Environment;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
 
 import com.alex.model.FileInfo;
 import com.alex.model.ThreadInfo;
+
+import org.apache.http.HttpStatus;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class DownloadService extends Service {
 
@@ -15,6 +26,25 @@ public class DownloadService extends Service {
 
     public static final String ACTION_START = "ACTION_START";
     public static final String ACTION_PAUSE = "ACTION_PAUSE";
+    public static final int MSG_INIT = 1;
+
+
+
+
+    public static final String DOWNLOAD_PATH = Environment.getExternalStorageDirectory() + "/download/";
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_INIT:
+                    FileInfo fileInfo = (FileInfo) msg.obj;
+                    Log.i(TAG,"handeMessage-->>length:"+fileInfo.getLength());
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
 
     public DownloadService() {
     }
@@ -24,6 +54,8 @@ public class DownloadService extends Service {
         if(ACTION_START.equals(intent.getAction())) {
             FileInfo fileInfo = (FileInfo) intent.getSerializableExtra("fileInfo");
             Log.i(TAG,"start:"+fileInfo.toString());
+            //启动初始化线程
+            new InitThread(fileInfo).start();
         } else if(ACTION_PAUSE.equals(intent.getAction())) {
             FileInfo fileInfo = (FileInfo) intent.getSerializableExtra("fileInfo");
             Log.i(TAG,"pause:"+fileInfo.toString());
@@ -37,21 +69,54 @@ public class DownloadService extends Service {
     }
 
     class InitThread extends Thread {
-        private ThreadInfo mThreadInfo = null;
+        private FileInfo mFileInfo;
 
-        InitThread(ThreadInfo mThreadInfo) {
-            this.mThreadInfo = mThreadInfo;
+        InitThread(FileInfo fileInfo) {
+            this.mFileInfo = fileInfo;
         }
 
         @Override
         public void run() {
+            HttpURLConnection conn = null;
+            RandomAccessFile raf = null;
             try {
                 //连接网络文件
-                //获得文件长度
+                URL url = new URL(mFileInfo.getUrl());
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setConnectTimeout(6*1000);
+                conn.setRequestMethod("GET");
+                int length = -1;
+                if(conn.getResponseCode() == HttpStatus.SC_OK) {
+                    //获得文件长度
+                    length = conn.getContentLength();
+                }
+                if(length <= 0) {
+                    return ;
+                }
+                File dir = new File(DOWNLOAD_PATH);
+                if(!dir.exists()) {
+                    dir.mkdirs();
+                }
                 //在本地创建文件
+                File file = new File(dir,mFileInfo.getFileName());
+                raf = new RandomAccessFile(file,"rwd");
                 //设置文件长度
+                raf.setLength(length);
+                mFileInfo.setLength(length);
+                mHandler.obtainMessage(MSG_INIT,mFileInfo).sendToTarget();
             } catch (Exception e) {
-
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (conn != null) {
+                        conn.disconnect();
+                    }
+                    if (raf != null) {
+                        raf.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
